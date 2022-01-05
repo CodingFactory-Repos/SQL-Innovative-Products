2fabb39f766b1a3a3c2997b67b450bbe45e3fe80
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2022-01-05 16:38:43 +0100

    Create insert_Brevet.sql

---
+++ PartA/Step2/insert_Brevet.sql
@@ -0,0 +1,5 @@
+INSERT INTO codingfactory_produits_innovants.Brevet (Nbrev, intitule, date, pays) VALUES (2, 'Lampe à LED', '2020-11-05', 'Etats-Unis');
+INSERT INTO codingfactory_produits_innovants.Brevet (Nbrev, intitule, date, pays) VALUES (3, 'Canapé lit', '2020-12-04', 'Belgique');
+INSERT INTO codingfactory_produits_innovants.Brevet (Nbrev, intitule, date, pays) VALUES (4, 'Brevet3', '2020-09-03', 'Norvège');
+INSERT INTO codingfactory_produits_innovants.Brevet (Nbrev, intitule, date, pays) VALUES (5, 'Brevet4', '2020-08-02', 'Maroc');
+INSERT INTO codingfactory_produits_innovants.Brevet (Nbrev, intitule, date, pays) VALUES (6, 'Brevet5', '2020-07-01', 'France');
